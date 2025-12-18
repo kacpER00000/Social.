f1ada5ea1861b71ef079710a91c17967b54662e7
Kacper <kurekkacper64@gmail.com>
2025-12-18 18:31:27 +0100

    Init commit

---
+++ backend/src/main/java/org/socialbackend/SocialBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.socialbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialBackEndApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/org/socialbackend/SocialBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.socialbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
