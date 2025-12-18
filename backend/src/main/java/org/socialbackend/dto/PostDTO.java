81ccb05d0e3ac8c731a0fbd2d3aaa0d5fab40935
Kacper <kurekkacper64@gmail.com>
2025-12-18 21:40:38 +0100

    refactored post_dto and user_dto to lombok

--- backend/src/main/java/org/socialbackend/dto/PostDTO.java
+++ backend/src/main/java/org/socialbackend/dto/PostDTO.java
@@ -1,7 +1,14 @@
 package org.socialbackend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PostDTO {
     private Long postId;
     private String author;
@@ -9,61 +16,4 @@ public class PostDTO {
     private String content;
     private LocalDateTime createdAt;
     private Long likesNum;
-
-    public PostDTO(Long postId, String author, String title, String content, LocalDateTime createdAt, Long likesNum) {
-        this.postId = postId;
-        this.author = author;
-        this.title = title;
-        this.content = content;
-        this.createdAt = createdAt;
-        this.likesNum = likesNum;
-    }
-
-    public Long getPostId() {
-        return postId;
-    }
-
-    public void setPostId(Long postId) {
-        this.postId = postId;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Long getLikesNum() {
-        return likesNum;
-    }
-
-    public void setLikesNum(Long likesNum) {
-        this.likesNum = likesNum;
-    }
 }
--- backend/src/main/java/org/socialbackend/dto/UserDTO.java
+++ backend/src/main/java/org/socialbackend/dto/UserDTO.java
@@ -1,59 +1,18 @@
 package org.socialbackend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
     private Long userId;
     private String firstName;
     private String lastName;
     private LocalDate birthDate;
     private Character sex;
-
-    public UserDTO(Long userId, String firstName, String lastName, LocalDate birthDate, Character sex) {
-        this.userId = userId;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.birthDate = birthDate;
-        this.sex = sex;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public Character getSex() {
-        return sex;
-    }
-
-    public void setSex(Character sex) {
-        this.sex = sex;
-    }
 }
