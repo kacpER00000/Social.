fa9cb3588216a156dd0a15926d2f406b76e477f0
Kacper <kurekkacper64@gmail.com>
2025-12-18 19:32:20 +0100

    created post entity

---
+++ backend/src/main/java/org/socialbackend/model/Post.java
@@ -0,0 +1,80 @@
+package org.socialbackend.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="post", schema="social")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="post_id")
+    private Long postId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    @JsonIgnore
+    private User user;
+    @Column(name="title")
+    private String title;
+    @Column(name="content")
+    private String content;
+    @Column(name="created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @OneToMany(mappedBy = "post")
+    private List<Comment> comments = new ArrayList<>();
+
+    public Post(){}
+
+    public Post(User user, String title, String content) {
+        this.user = user;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void addComment(Comment comment){
+        comments.add(comment);
+        comment.setPost(this);
+    }
+    public void removeComment(Comment comment){
+        comments.remove(comment);
+        comment.setPost(this);
+    }
+}
