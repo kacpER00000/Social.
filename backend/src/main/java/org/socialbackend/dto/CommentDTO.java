fcb44291470af23973f91ebe7f3fa45a02a9b096
Kacper <kurekkacper64@gmail.com>
2025-12-18 21:37:04 +0100

    created comment dto

---
+++ backend/src/main/java/org/socialbackend/dto/CommentDTO.java
@@ -0,0 +1,18 @@
+package org.socialbackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentDTO {
+    private Long commentId;
+    private Long postId;
+    private String author;
+    private String content;
+    private LocalDateTime createdAt;
+}
