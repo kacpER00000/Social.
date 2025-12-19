fe1f56c7cda88de5405ad91e4c002d5babe26925
Kacper <kurekkacper64@gmail.com>
2025-12-19 19:11:52 +0100

    updated comment request

--- backend/src/main/java/org/socialbackend/request/CreateCommentRequest.java
+++ backend/src/main/java/org/socialbackend/request/CreateCommentRequest.java
@@ -10,6 +10,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateCommentRequest {
+    @NotNull
+    private Long userId;
     @NotBlank
     private String content;
 }
