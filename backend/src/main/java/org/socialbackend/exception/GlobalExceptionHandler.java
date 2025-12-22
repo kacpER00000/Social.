6353173c8e705d58e8a732a8c1fb9d77b79d632c
Kacper <kurekkacper64@gmail.com>
2025-12-22 13:00:25 +0100

    created global exception handler for rest controllers

---
+++ backend/src/main/java/org/socialbackend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package org.socialbackend.exception;
+
+import org.apache.coyote.Response;
+import org.apache.tomcat.util.http.InvalidParameterException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNotFound(NoSuchElementException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<String> handleIllegalState(IllegalStateException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+
+    }
+    @ExceptionHandler(InvalidParameterException.class)
+    public ResponseEntity<String> handleInvalidParameter(InvalidParameterException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+}
