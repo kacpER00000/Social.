1b8806c7540aab9288fb4f791f9a7cd10ed5c72e
Kacper <kurekkacper64@gmail.com>
2025-12-29 17:09:43 +0100

    added handlers for followers to the controller

--- backend/src/main/java/org/socialbackend/controller/UserController.java
+++ backend/src/main/java/org/socialbackend/controller/UserController.java
@@ -2,10 +2,17 @@ package org.socialbackend.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.socialbackend.dto.FollowerDTO;
 import org.socialbackend.dto.UserDTO;
+import org.socialbackend.repository.FollowerRepository;
 import org.socialbackend.request.RegisterUserRequest;
 import org.socialbackend.request.UpdateUserRequest;
+import org.socialbackend.service.FollowerService;
 import org.socialbackend.service.UserService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +25,7 @@ import java.util.NoSuchElementException;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final FollowerService followerService;
 
     @GetMapping("/{userId}")
     public ResponseEntity<UserDTO> getUser(@PathVariable Long userId){
@@ -41,5 +49,23 @@ public class UserController {
     @GetMapping("/search")
     public ResponseEntity<List<UserDTO>> findUsersByFirstNameOrLastName(@RequestParam String query){
         return ResponseEntity.ok(userService.findUsersByFirstNameOrLastName(query));
+    }
+    @PostMapping("/{targetUserId}/follow")
+    public ResponseEntity<Void> follow(@PathVariable Long targetUserId, @RequestParam Long followerId){
+        followerService.follow(followerId, targetUserId);
+        return ResponseEntity.ok().build();
+    }
+    @DeleteMapping("/{targetUserId}/follow")
+    public ResponseEntity<Void> unfollow(@PathVariable Long targetUserId, @RequestParam Long followerId){
+        followerService.unfollow(followerId, targetUserId);
+        return ResponseEntity.noContent().build();
+    }
+    @GetMapping("/{userId}/followers")
+    public ResponseEntity<Page<FollowerDTO>> getUserFollowers(@PathVariable Long userId, @PageableDefault Pageable pageable){
+        return ResponseEntity.ok(followerService.findUserFollowers(userId, pageable));
+    }
+    @GetMapping("/{userId}/following")
+    public ResponseEntity<Page<FollowerDTO>> getUserFollowing(@PathVariable Long userId, @PageableDefault Pageable pageable){
+        return ResponseEntity.ok(followerService.findUserFollowing(userId, pageable));
     }
 }
