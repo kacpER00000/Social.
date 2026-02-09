86ef261c0edf367f00bdba7774a69f6f1e921191
Kacper <kurekkacper64@gmail.com>
2026-02-09 18:48:31 +0100

    add: add redirect to login page after successful registration

--- frontend/social/src/components/Register.tsx
+++ frontend/social/src/components/Register.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
     const [sex, setSex] = useState("M");
@@ -14,6 +15,7 @@ const Register = () => {
     const [birthDateError, setBirthDateError] = useState(false);
     const [accountExistenceError, setAccountExistenceError] = useState(false);
     const [loading, setLoadingState] = useState(false)
+    const navigate = useNavigate();
     const today = new Date();
     today.setFullYear(today.getFullYear() - 18);
     const maxDate = today.toISOString().split('T')[0];
@@ -58,13 +60,13 @@ const Register = () => {
             })
             if (response.ok) {
                 setAccountExistenceError(false)
-                //TODO: Redirect to Login
+                navigate("/login")
             }
             if(response.status === 400){
                 triggerAccountExistenceError()
             }
         } catch (e) {
-            console.log("Network error: ", e)
+            console.log("Error: ", e)
         } finally {
             setLoadingState(false)
         }
@@ -171,7 +173,7 @@ const Register = () => {
                                     <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                                     <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                                 </svg>
-                            :
+                                :
                                 "Register"
                             }
                         </button>
