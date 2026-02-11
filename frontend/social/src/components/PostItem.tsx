3accdddb0259dc28e9c87eb6b1e9d26ba171bc1a
Kacper <kurekkacper64@gmail.com>
2026-02-11 18:39:46 +0100

    add: add navigate to page with single post

--- frontend/social/src/components/PostItem.tsx
+++ frontend/social/src/components/PostItem.tsx
@@ -1,17 +1,19 @@
 import {Post} from "./types/types.ts";
+import {useNavigate} from "react-router-dom";
 
 type PostProps = {
     post: Post
 }
 
 const PostItem=({post}: PostProps)=>{
+    const navigate = useNavigate()
     return(
-        <div className="border rounded-3xl p-5 m-3">
+        <div className="shadow-xl rounded-3xl p-5 m-3 cursor-pointer" onClick={() => {navigate(`/posts/${post.postId}`)}}>
             <div>
                 <p className="font-bold">{post.author}</p>
                 <p className="text-xs">{post.createdAt}</p>
                 <hr/>
-                <p>{post.content}</p>
+                <p>{post.title}</p>
                 <hr/>
             </div>
             <div className="flex justify-between">
