8345c489a498559c0a0ca864403d27d739e93d7d
Kacper <kurekkacper64@gmail.com>
2026-02-16 22:06:33 +0100

    feature: create reusable follow button

---
+++ frontend/social/src/components/FollowButton.tsx
@@ -0,0 +1,12 @@
+type FollowButtonProps = {
+    isFollowing: boolean,
+    handleFollow: () => void
+}
+
+const FollowButton = ({isFollowing, handleFollow}: FollowButtonProps) => {
+    return(
+        <button className={`${isFollowing ? "bg-gray-300 text-black hover:bg-gray-400" : "bg-blue-500 text-white hover:bg-blue-600" } text-white rounded-full px-6 py-2 mb-6 transition-colors duration-300 ease-in-out font-bold text-sm`} onClick={handleFollow}>{isFollowing ? "Unfollow": "Follow"}</button>
+    );
+}
+
+export default FollowButton;
