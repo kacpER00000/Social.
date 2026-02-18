a14dc57b969921c2adf3ac781131df45824a7535
Kacper <kurekkacper64@gmail.com>
2026-02-18 15:23:11 +0100

    refactor: refactor Home to use generic Post list component and adjust PostItem component with profile feature

--- frontend/social/src/components/PostItem.tsx
+++ frontend/social/src/components/PostItem.tsx
@@ -1,29 +1,32 @@
-import {Post} from "../types/types.ts";
+import {PostDTO} from "../types/types.ts";
 import {useNavigate} from "react-router-dom";
 
 type PostProps = {
-    post: Post
+    post: PostDTO,
+    onSelect: (post: PostDTO) => void
 }
 
-const PostItem=({post}: PostProps)=>{
-    const navigate = useNavigate()
+const PostItem=({post, onSelect}: PostProps)=>{
+    const navigate = useNavigate();
     return(
-        <div className="shadow-xl rounded-3xl p-5 m-3 cursor-pointer" onClick={() => {navigate(`/posts/${post.postId}`)}}>
+        <div className="shadow-xl rounded-3xl p-5 m-3 cursor-pointer">
             <div>
-                <p className="font-bold">{post.author}</p>
+                <p className="font-bold hover:underline" onClick={() => {navigate(`/profile/${post.authorId}`)}}>{post.author}</p>
+            </div>
+            <div onClick={() => {onSelect(post)}}>
                 <p className="text-xs">{post.createdAt}</p>
                 <hr/>
                 <p>{post.title}</p>
                 <hr/>
-            </div>
-            <div className="flex justify-between">
-                <div>
-                    {post.likesNum}
-                    <i className="icon-thumbs-up-alt"></i>
-                </div>
-                <div>
-                    {post.commentCount}
-                    <i className="icon-comment"></i>
+                <div className="flex justify-between">
+                    <div>
+                        {post.likesNum}
+                        <i className="icon-thumbs-up-alt"></i>
+                    </div>
+                    <div>
+                        {post.commentCount}
+                        <i className="icon-comment"></i>
+                    </div>
                 </div>
             </div>
         </div>
