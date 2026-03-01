feff12ce3077929a7f7878ff62d59ebdcf933d2a
Kacper <kurekkacper64@gmail.com>
2026-03-01 17:37:32 +0100

    refactor: merge FollowStatus and FollowerDTO into one dto, refactor mapToDTO method in FollowerService to use refactored FollowerDTO and refactor InspectCard to use new FollowerDTO

--- backend/src/main/java/org/socialbackend/controller/UserController.java
+++ backend/src/main/java/org/socialbackend/controller/UserController.java
@@ -2,27 +2,19 @@ package org.socialbackend.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.socialbackend.details.AppUserDetails;
-import org.socialbackend.dto.FollowStatus;
 import org.socialbackend.dto.FollowerDTO;
 import org.socialbackend.dto.UserDTO;
-import org.socialbackend.model.User;
-import org.socialbackend.repository.FollowerRepository;
-import org.socialbackend.request.RegisterUserRequest;
 import org.socialbackend.request.UpdateUserRequest;
 import org.socialbackend.service.FollowerService;
 import org.socialbackend.service.UserService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/social/users")
@@ -32,50 +24,63 @@ public class UserController {
     private final FollowerService followerService;
 
     @GetMapping("/{userId}")
-    public ResponseEntity<UserDTO> getUser(@PathVariable Long userId, Authentication authentication){
+    public ResponseEntity<UserDTO> getUser(@PathVariable Long userId, Authentication authentication) {
         AppUserDetails userDetails = (AppUserDetails) authentication.getPrincipal();
         return ResponseEntity.ok(userService.findUserById(userId, userDetails.getUserId()));
     }
 
     @PutMapping()
-    public ResponseEntity<Void> updateUser(@Valid @RequestBody UpdateUserRequest updateUserRequest, Authentication authentication){
+    public ResponseEntity<Void> updateUser(@Valid @RequestBody UpdateUserRequest updateUserRequest,
+            Authentication authentication) {
         AppUserDetails userDetails = (AppUserDetails) authentication.getPrincipal();
         userService.updateUser(userDetails.getUserId(), updateUserRequest);
         return ResponseEntity.ok().build();
     }
+
     @DeleteMapping()
-    public ResponseEntity<Void> deleteUser(Authentication authentication){
+    public ResponseEntity<Void> deleteUser(Authentication authentication) {
         AppUserDetails userDetails = (AppUserDetails) authentication.getPrincipal();
         userService.deleteUser(userDetails.getUserId());
         return ResponseEntity.noContent().build();
     }
+
     @GetMapping("/search")
-    public ResponseEntity<List<UserDTO>> findUsersByFirstNameOrLastName(@RequestParam String query, Authentication authentication){
+    public ResponseEntity<List<UserDTO>> findUsersByFirstNameOrLastName(@RequestParam String query,
+            Authentication authentication) {
         AppUserDetails userDetails = (AppUserDetails) authentication.getPrincipal();
         return ResponseEntity.ok(userService.findUsersByFirstNameOrLastName(query, userDetails.getUserId()));
     }
+
     @PostMapping("/{targetUserId}/follow")
-    public ResponseEntity<Void> follow(@PathVariable Long targetUserId, Authentication authentication){
+    public ResponseEntity<Void> follow(@PathVariable Long targetUserId, Authentication authentication) {
         AppUserDetails userDetails = (AppUserDetails) authentication.getPrincipal();
         followerService.follow(userDetails.getUserId(), targetUserId);
         return ResponseEntity.ok().build();
     }
+
     @DeleteMapping("/{targetUserId}/follow")
-    public ResponseEntity<Void> unfollow(@PathVariable Long targetUserId, Authentication authentication){
+    public ResponseEntity<Void> unfollow(@PathVariable Long targetUserId, Authentication authentication) {
         AppUserDetails userDetails = (AppUserDetails) authentication.getPrincipal();
         followerService.unfollow(userDetails.getUserId(), targetUserId);
         return ResponseEntity.noContent().build();
     }
+
     @GetMapping("/{userId}/followers")
-    public ResponseEntity<Page<FollowerDTO>> getUserFollowers(@PathVariable Long userId,@PageableDefault Pageable pageable){
-        return ResponseEntity.ok(followerService.findUserFollowers(userId, pageable));
+    public ResponseEntity<Page<FollowerDTO>> getUserFollowers(@PathVariable Long userId,
+            @PageableDefault Pageable pageable, Authentication authentication) {
+        AppUserDetails userDetails = (AppUserDetails) authentication.getPrincipal();
+        return ResponseEntity.ok(followerService.findUserFollowers(userId, userDetails.getUserId(), pageable));
     }
+
     @GetMapping("/{userId}/following")
-    public ResponseEntity<Page<FollowerDTO>> getUserFollowing(@PathVariable Long userId,@PageableDefault Pageable pageable){
-        return ResponseEntity.ok(followerService.findUserFollowing(userId, pageable));
+    public ResponseEntity<Page<FollowerDTO>> getUserFollowing(@PathVariable Long userId,
+            @PageableDefault Pageable pageable, Authentication authentication) {
+        AppUserDetails userDetails = (AppUserDetails) authentication.getPrincipal();
+        return ResponseEntity.ok(followerService.findUserFollowing(userId, userDetails.getUserId(), pageable));
     }
+
     @GetMapping("/{userId}/follow-status")
-    public ResponseEntity<FollowStatus> getFollowStatus(@PathVariable Long userId, Authentication authentication){
+    public ResponseEntity<FollowerDTO> getFollowStatus(@PathVariable Long userId, Authentication authentication) {
         AppUserDetails userDetails = (AppUserDetails) authentication.getPrincipal();
         return ResponseEntity.ok(followerService.getFollowInfo(userId, userDetails.getUserId()));
     }
--- backend/src/main/java/org/socialbackend/repository/FollowerRepository.java
+++ backend/src/main/java/org/socialbackend/repository/FollowerRepository.java
@@ -9,9 +9,12 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface FollowerRepository extends JpaRepository<Follower,Long> {
+public interface FollowerRepository extends JpaRepository<Follower, Long> {
     boolean existsByFollower_UserIdAndFollowed_UserId(Long followerId, Long followedId);
+
     Optional<Follower> findByFollower_UserIdAndFollowed_UserId(Long followerId, Long followedId);
-    Page<Follower> findAllByFollower_UserId(Long followerId,Pageable pageable);
-    Page<Follower> findAllByFollowed_UserId(Long followedId,Pageable pageable);
+
+    Page<Follower> findAllByFollower_UserId(Long followerId, Pageable pageable);
+
+    Page<Follower> findAllByFollowed_UserId(Long followedId, Pageable pageable);
 }
