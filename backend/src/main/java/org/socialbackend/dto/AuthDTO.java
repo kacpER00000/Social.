e9feb03fdc155df5310399d83675bb5c7e14fb25
Kacper <kurekkacper64@gmail.com>
2026-03-19 22:33:48 +0100

    refactor: refactor AuthDTO to use lombok

--- backend/src/main/java/org/socialbackend/dto/AuthDTO.java
+++ backend/src/main/java/org/socialbackend/dto/AuthDTO.java
@@ -1,4 +1,12 @@
 package org.socialbackend.dto;
 
-public record AuthDTO(String token) {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthDTO {
+    private String token;
 }
